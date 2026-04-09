b0051271c1261a53949f357645d298f8357d094c
Manas Kumar Jagyasi <145320344+Manas2353rr@users.noreply.github.com>
2026-04-10 00:48:32 +0530

    fix

--- apps/backend/api/index.js
+++ apps/backend/api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
+const cors = require("cors");
 
 const authRoutes         = require("../src/routes/auth.routes");
 const geoRoutes          = require("../src/routes/geo.routes");
@@ -13,15 +14,13 @@ const usageTracker       = require("../src/middleware/usageTracker");
 const app = express();
 
 // CORS headers manually — works on all Vercel deployments
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://india-geo-saas-obt4.vercel.app , https://india-geo-saas-obt4-hr3nbvqkn-manas-s-projects.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-API-Key");
-  res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") return res.status(200).end();
-  next();
-});
-
+app.use(cors({
+  origin: [
+    "https://india-geo-saas-obt4.vercel.app",
+    "https://india-geo-saas-obt4-hr3nbvqkn-manas-s-projects.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use((req, res, next) => {
