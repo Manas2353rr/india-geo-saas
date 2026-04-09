327f51d61b76c9360ad6273d691a174f3a29859c
Manas Kumar Jagyasi <145320344+Manas2353rr@users.noreply.github.com>
2026-04-09 18:14:41 +0530

    fix

--- apps/backend/api/index.js
+++ apps/backend/api/index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Credentials", "false");
+  res.header("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") return res.status(200).end();
   next();
 });
